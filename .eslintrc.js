2114eba2b5fd025ae7d72bc4358b2cef6cea6887
Dziedzic <michal.dziedzic22@gmail.com>
2020-03-21 18:06:39 +0100

    linting

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'rules': {
+    "comma-dangle": ["error", {
+      "arrays": "never",
+      "objects": "never",
+      "imports": "never",
+      "exports": "never",
+      "functions": "never"
+    }],
+    "require-jsdoc":0,
+    "max-len": ["error", { "code": 120 }]
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,38 +1,20 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
-import GameScene from "./GameScene";
-import StartScene from "./StartScene";
+import Phaser from 'phaser';
+import GameScene from './GameScene';
+import StartScene from './StartScene';
 
 
 const config = {
-    type: Phaser.AUTO,
-    parent: "phaser-example",
-    width: window.innerWidth - 10,
-    height: window.innerHeight - 20,
-    scene: [
-        StartScene,
-        GameScene
-    ],
-    physics: {
-        default: "arcade"
-    }
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: window.innerWidth - 10,
+  height: window.innerHeight - 20,
+  scene: [
+    StartScene,
+    GameScene
+  ],
+  physics: {
+    default: 'arcade'
+  }
 };
 
-const game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image("logo", logoImg);
-}
-
-function create() {
-    const logo = this.add.image(400, 150, "logo");
-
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: "Power2",
-        yoyo: true,
-        loop: -1
-    });
-}
+new Phaser.Game(config);
